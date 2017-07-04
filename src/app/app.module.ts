855e7bc27f7c19e563fa3b40d1a38e315611cb7a
popsapple <saysun34@gmail.com>
2017-07-05 04:18:00 +0900

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { MainVisualComponent } from './app.component';
 import { MainVisualItemComponent } from './main_visual/mainvisual.component';
+import { PortfolioListComponent } from './portfolio/portfolio_list.component';
 import { GnbComponent } from './gnb/gnb.component';
-
 @NgModule({
   declarations: [
-    MainVisualComponent,MainVisualItemComponent,GnbComponent
+    MainVisualComponent,MainVisualItemComponent,GnbComponent,PortfolioListComponent
   ],
   imports: [
     BrowserModule

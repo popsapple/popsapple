f2abae6f708f5c64e93d64d5eb83a10f1d31d38c
popsapple <saysun34@gmail.com>
2017-07-11 08:38:15 +0900

    프로필까지 작업

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,24 @@ import { MainVisualComponent } from './app.component';
 import { MainVisualItemComponent } from './main_visual/mainvisual.component';
 import { PortfolioListComponent } from './portfolio/portfolio_list.component';
 import { PortfolioChartComponent } from './portfolio/portfolio_chart.component';
+import { MainProfileMe } from './main_profile/mainprofile.component';
+import { MainSkillList } from './skill_list/skilllist.component';
+import { MainSkillListService } from './skill_list/skilllist.service';
 import { BaseChartDemoComponent } from './chart/chart.component';
 import { GnbComponent } from './gnb/gnb.component';
 import { ChartsModule } from 'ng2-charts';
 enableProdMode();
 @NgModule({
   declarations: [
-    MainVisualComponent,MainVisualItemComponent,GnbComponent,PortfolioListComponent,PortfolioChartComponent,BaseChartDemoComponent
+    MainVisualComponent,
+    MainVisualItemComponent,
+    GnbComponent,
+    PortfolioListComponent,
+    PortfolioChartComponent,
+    BaseChartDemoComponent,
+    MainProfileMe,
+    MainSkillList,
+    MainSkillListService
   ],
   imports: [
     BrowserModule,ChartsModule
--- src/assets/js/movefollitem.js
+++ src/assets/js/movefollitem.js
@@ -133,8 +133,9 @@ $.fn.onMovingFllowingItem = function(options){
     });
     img.src = img_src;
     img.onload = function(){
-      console.log("이미지 전부 로드됨");
-      obj.check_item_width();
+      setTimeout(function(){
+        obj.check_item_width();
+      },1000);
     };
   }else{
     obj.check_item_width();

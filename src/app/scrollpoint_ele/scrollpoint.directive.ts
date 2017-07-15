a0dea85ebce6cc891f17b0d319a90dac90942fa5
popsapple <saysun34@gmail.com>
2017-07-16 02:36:44 +0900

    스크롤이랑 엘리먼트 top 맟추는 디렉티브 추가

---
+++ src/app/scrollpoint_ele/scrollpoint.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostListener, ViewChild, AfterViewInit, ElementRef, Renderer } from '@angular/core';
+@Directive({
+  selector: '[scroll-check]'
+})
+export class ScrollPointCheckDirective {
+  active_class:string;
+  offset_top:number;
+  window_top:number;
+  class_list:string;
+  constructor(private _el:ElementRef,private renderer:Renderer){
+    this.active_class = "scroll_active";
+    this.offset_top = 0;
+  }
+  //@HostListener('click', ['$event'])
+  //c_onEnterrr($event) {
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll($event) {
+    this.offset_top = this._el.nativeElement.offsetTop;
+    this.window_top = $event.currentTarget.scrollY || $event.currentTarget.pageYOffset;
+    this.class_list = this._el.nativeElement.getAttribute('class');
+    if(this.active_class == undefined){
+      this.active_class = this.class_list+" scroll_none";
+    }
+    if(this.window_top > (this.offset_top-(this.offset_top/5))){
+      this.renderer.setElementAttribute(this._el.nativeElement, 'class', this.active_class);
+    }
+  }
+}

459a5f8128f081f0ad37e4c72d344f76ffaa7236
popsapple <saysun34@gmail.com>
2017-07-24 01:37:17 +0900

    .

--- src/app/admin/portfolio_write/portfolio_post.service.ts
+++ src/app/admin/portfolio_write/portfolio_post.service.ts
@@ -30,18 +30,25 @@ export class PortfolioPostService {
     // 브라우저가 자동 지정하므로 컨텐츠 타입 헤더를 지정하면 전송이 되지 않는다.... -_-....
     // console.log("업로드 파일 정보 :: "+JSON.stringify(inputElele));
     console.log("업로드 파일 정보 02 :: "+JSON.stringify(inputEl.nativeElement.files[0]));
-    var formData = new FormData(inputEl.nativeElement.files[0]);
-    console.log("업로드 파일 정보 03 :: "+JSON.stringify(formData));
-    this.http.put('/upload_thumnail', {
-      file: formData // new FormData(); with image file appended.
-    }).subscribe(
-      data => {
-        this.PortfolioPostComponent.ThumfileUpload(data.json().message).subscribe(():void => {
-        });
-      },error => {
-        alert('다시 전송해주세요. :: '+error);
-      },() => {
-    });
+    let inputElE: HTMLInputElement = inputEl.nativeElement;
+    let fileCount: number = inputElE.files.length;
+    let formData = new FormData();
+    if (fileCount > 0) { // a file was selected
+      for (let i = 0; i < fileCount; i++) {
+          formData.append('file[]', inputElE.files.item(i));
+      }
+      console.log("진입");
+      this.http.put('/upload_thumnail', {
+        file: formData // new FormData(); with image file appended.
+      }).subscribe(
+        data => {
+          this.PortfolioPostComponent.ThumfileUpload(data.json().message).subscribe(():void => {
+          });
+        },error => {
+          alert('다시 전송해주세요. :: '+error);
+        },() => {
+      });
+    }
   }
   public getPortfolioPostValue() {
     let that = this;

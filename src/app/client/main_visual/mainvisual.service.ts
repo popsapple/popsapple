f69a44d99471734c0df5bbc082ce811003306203
popsapple <saysun34@gmail.com>
2017-08-03 00:29:19 +0900

    메인 비주얼 모바일에 맞게 사이즈 조정

--- src/app/client/main_visual/mainvisual.service.ts
+++ src/app/client/main_visual/mainvisual.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 @Injectable()
 export class MainVisualService {
-  getImages(main_visual_img:string,main_visual_desc:string,main_visual_title_md:string,main_visual_title:string,main_visual_title_eng:string){
+  getImages(main_visual_img_mobile:string,main_visual_img:string,main_visual_desc:string,main_visual_title_md:string,main_visual_title:string,main_visual_title_eng:string){
     return {
+      main_visual_img_mobile : main_visual_img_mobile,
       main_visual_img : main_visual_img,
       main_visual_desc : main_visual_desc,
       main_visual_title_md : main_visual_title_md,

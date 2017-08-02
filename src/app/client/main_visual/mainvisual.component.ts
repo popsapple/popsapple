192f7a00cd8e15f15c8a2bffeb29c605e9373fed
popsapple <saysun34@gmail.com>
2017-08-03 02:08:28 +0900

    .

--- src/app/client/main_visual/mainvisual.component.ts
+++ src/app/client/main_visual/mainvisual.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 import { MainVisualService } from '../main_visual/mainvisual.service';
 import { LoadTemplateScript } from './../../lib/loadjs/loadscript.service';
 @Component({
@@ -7,13 +7,13 @@ import { LoadTemplateScript } from './../../lib/loadjs/loadscript.service';
   styleUrls: ['./../../../assets/css/mainvisual.component.compact.css'],
   providers: [MainVisualService,LoadTemplateScript]
 })
-export class MainVisualItemComponent implements OnInit {
+export class MainVisualItemComponent implements AfterViewInit {
   main_visual_item01:Object;
   main_visual_item02:Object;
   main_visual_script:any;
   main_visual_items:Array<any> = [];
   constructor(){}
-  ngOnInit(){
+  ngAfterViewInit(){
     this.main_visual_item01 = new MainVisualService().getImages('./../../assets/image/visual/main_visual01_m.jpg','./../../assets/image/visual/main_visual01.jpg', '데스크탑 위에서 작업 중인 사진입니다', "자신의 일을 개선시키려면 불가능을 시도해라", "불가능을 시도해라", 'Attempt the Inpossible in order to improve your work');
     this.main_visual_item02 = new MainVisualService().getImages('./../../assets/image/visual/main_visual02_m.jpg','./../../assets/image/visual/main_visual02.jpg', '데스크탑 위에서 작업 중인 사진입니다', "자신의 일을 개선시키려면 불가능을 시도해라", "불가능을 시도해라", 'Attempt the Inpossible in order to improve your work');
     this.main_visual_items.push(this.main_visual_item01);
--- src/app/client/main_visual/mainvisual.service.ts
+++ src/app/client/main_visual/mainvisual.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MainVisualService {
   getImages(main_visual_img_mobile:string,main_visual_img:string,main_visual_desc:string,main_visual_title_md:string,main_visual_title:string,main_visual_title_eng:string){
-    console.log ("?? :: "+main_visual_img_mobile);
     return {
       main_visual_img_mobile : main_visual_img_mobile,
       main_visual_img : main_visual_img,

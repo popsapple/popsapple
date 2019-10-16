131f1cc579102de4cc61aaa1952ef6b65eb33ed9
popsapple <saysun34@gmail.com>
2019-10-16 14:30:33 +0900

    로딩전 attr 변경 금지

--- src/app/client/chart/chart.component.ts
+++ src/app/client/chart/chart.component.ts
@@ -1,9 +1,17 @@
-import { Component, Directive, Input, ViewChild, AfterViewInit,ElementRef,Renderer } from '@angular/core';
-import { BaseChartDirective } from 'ng2-charts';
+import {
+  Component,
+  Directive,
+  Input,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  Renderer
+} from "@angular/core";
+import { BaseChartDirective } from "ng2-charts";
 @Component({
-selector: 'base-chart',
-templateUrl: './chart-component.html',
-styleUrls: ['./../../../assets/css/portfolio_chart.component.compact.css']
+  selector: "base-chart",
+  templateUrl: "./chart-component.html",
+  styleUrls: ["./../../../assets/css/portfolio_chart.component.compact.css"]
 })
 export class BaseChartDemoComponent implements AfterViewInit {
   @ViewChild(BaseChartDirective) public _chart;
@@ -11,45 +19,51 @@ export class BaseChartDemoComponent implements AfterViewInit {
   @Input() chartdata2 = "500";
   @Input() type1 = "normal";
 
-  public pieChartType:string = 'doughnut';
-  public value_one:any;
-  public value_two:any;
+  public pieChartType: string = "doughnut";
+  public value_one: any;
+  public value_two: any;
   // Pie
-  public pieChartLabels:string[] = ['dose', 'dosenot'];
+  public pieChartLabels: string[] = ["dose", "dosenot"];
   public chartColors: any[] = [
-        {
-          backgroundColor: ['rgba(255,255,255,1)','rgba(180,180,180,0.5)'],
-          borderColor: 'rgba(0,0,0,0)',
-          pointBackgroundColor: 'rgba(0,0,0,0)',
-          pointBorderColor: 'rgba(0,0,0,0)',
-          pointHoverBackgroundColor: 'rgba(0,0,0,0)',
-          pointHoverBorderColor: 'rgba(0,0,0,0)',
-          animateRotate: false
-        }];
-  public chartOptions:any = {
+    {
+      backgroundColor: ["rgba(255,255,255,1)", "rgba(180,180,180,0.5)"],
+      borderColor: "rgba(0,0,0,0)",
+      pointBackgroundColor: "rgba(0,0,0,0)",
+      pointBorderColor: "rgba(0,0,0,0)",
+      pointHoverBackgroundColor: "rgba(0,0,0,0)",
+      pointHoverBorderColor: "rgba(0,0,0,0)",
+      animateRotate: false
+    }
+  ];
+  public chartOptions: any = {
     legend: {
       display: false
     },
     tooltips: {
       enabled: false
     }
-  }
+  };
 
-  constructor(private el: ElementRef) {
-  }
+  constructor(private el: ElementRef) {}
 
-  public randomizeType():void {
-    this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+  public randomizeType(): void {
+    this.pieChartType = this.pieChartType === "doughnut" ? "pie" : "doughnut";
   }
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
 
-  public chartHovered(e:any):void {
+  public chartHovered(e: any): void {
     this._chart.refresh();
   }
-  ngAfterViewInit(){
-    this.el.nativeElement.querySelector("iframe").setAttribute("aria-hidden", "true");
-    this.el.nativeElement.querySelector("iframe").setAttribute("title", "Chart");
+  ngAfterViewInit() {
+    if (this.el.nativeElement.querySelector("iframe")) {
+      this.el.nativeElement
+        .querySelector("iframe")
+        .setAttribute("aria-hidden", "true");
+      this.el.nativeElement
+        .querySelector("iframe")
+        .setAttribute("title", "Chart");
+    }
   }
 }
